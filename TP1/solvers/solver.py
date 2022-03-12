f9bf7712c2ca17475d6f4b2667ab03567defe3e7
patrickmdey <patrickmdey@hotmail.com>
2022-03-12 19:05:43 -0300

    Check if a puzzle is resolvable beforehand
    Co-authored-by: srosati <srosati@itba.edu.ar>
    Co-authored-by: matiaslombardi <mflombardi2000@gmail.com>

--- TP1/solvers/solver.py
+++ TP1/solvers/solver.py
@@ -28,13 +28,22 @@ class Solver:
 
     def solve(self):
         start_time = time.time()
+        print("Solving for puzzle:")
+        self.tree.get_root().get_state().print_board()
+        print()
+        if not self.is_solvable(self.tree.get_root().get_state().get_board()):
+            print("--- %s Seconds ---" % (time.time() - start_time))
+            print("There is no POSSIBLE solution for this initial configuration")
+            return []
+
         bisect.insort(self.frontier, self.tree.get_root())
         while len(self.frontier) > 0:
             curr_node = self.frontier.pop(0)
             curr_state = curr_node.get_state()
 
             if str(curr_state.get_board()) not in self.explored:
-                self.explored[str(curr_state.get_board())] = curr_node  # TODO stringify de otra manera
+                # TODO stringify de otra manera
+                self.explored[str(curr_state.get_board())] = curr_node
 
             if curr_state.is_solution():
                 self.print_stats(start_time, curr_node)
@@ -56,7 +65,7 @@ class Solver:
         self.print_stats(start_time)
         return []
 
-    def print_stats(self, start_time, node = None):
+    def print_stats(self, start_time, node=None):
         print("--- %s Seconds ---" % (time.time() - start_time))
         if node is None:
             print('There is no solution')
@@ -66,3 +75,29 @@ class Solver:
             print('Depth: ' + str(node.get_cost()))
         print('Frontier nodes: ' + str(len(self.frontier)))
         print('Explored nodes: ' + str(len(self.explored)))
+
+    # source: https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
+    # A utility function to count
+    # inversions in given array 'arr[]'
+
+    @staticmethod
+    def get_inv_count(arr):
+        inv_count = 0
+        empty_value = 0
+        for i in range(0, 9):
+            for j in range(i + 1, 9):
+                if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
+                    inv_count += 1
+        return inv_count
+
+    # This function returns true
+    # if given 8 puzzle is solvable.
+
+    def is_solvable(self, puzzle):
+        # Count inversions in given 8 puzzle
+        inv_count = self.get_inv_count([j for sub in puzzle for j in sub])
+        # return true if inversion count is even.
+        return inv_count % 2 == 0
+
+    # This code is contributed by vitorhugooli
+    # Fala meu povo desse Brasil varonil
