60ac1319969b244119f0c0ec6f1c0801f5b7e61d
matiaslombardi <mflombardi2000@gmail.com>
2022-04-21 18:54:41 -0300

    fixes to simple perceptron
    Co-authored-by: patrickmdey <patrickmdey@hotmail.com>
    Co-authored-by: srosati <srosati@itba.edu.ar>

--- TP3/activation.py
+++ TP3/activation.py
@@ -1,18 +1,18 @@
-import numpy as np
 import math
 
 
 class Activation:
     @staticmethod
     def simple(h, u):
-        if h-u < 0: return -1
+        if h - u < 0:
+            return -1
         return 1
-    
+
     @staticmethod
     def lineal(h, u):
         return h - u
-    
+
     @staticmethod
     def not_lineal(h, u, b):
         x = h, u
-        return math.tanh(b*x)
+        return math.tanh(b * x)
--- TP3/simple_perceptron.py
+++ TP3/simple_perceptron.py
@@ -1,42 +1,51 @@
 import numpy as np
 import math
+
+
 class SimplePerceptron:
-    def __init__(self, cota, error, umbral) -> None:
-        self.cota = cota
+    def __init__(self, limit, error, threshold) -> None:
+        self.limit = limit
         self.error = error
-        self.umbral = umbral
-    def solve(self, in_set, out_set, activation_func): #inset: x, out_set: y 
-        p = len(in_set)
-        i = 0
-        n = 0.1 #tasa de aprendizaje
-        w = [0,0] ##np.zeros(n) usa float64 y se rompe todo. ## el umbral se lo paso al pereceptron 
-        delta_w = w
-        min_error = p*2
-        while self.error > 0 and i <self.cota:
-            i = i+1
-            i_x = np.random.randint(1, p)
-            h = self.calculate_h(in_set[i_x], w)
-            o = activation_func(h,self.umbral)
-            for j in range(0, len(w)):  
-                delta_w[j] = n * (out_set[i_x] - o) * in_set[i_x][0] # aca no entiendo que x[i_x] tomar porque seria un array [-1, 1] ponele
-                w[j] = w[j] + delta_w[j]
-            self.error = self.calculate_error(in_set, out_set, w, p)
+        self.threshold = threshold
+
+    def solve(self, training_set, correct_output, activation_func):  # inset: x, correct_output: y
+        p = len(training_set)
+        iteration = 0
+        n = 0.1  # tasa de aprendizaje
+        w = np.zeros(len(training_set[0]))  # np.zeros(n) usa float64 y se rompe
+        delta_w = np.zeros(len(training_set[0]))
+        self.error = 1
+        min_error = p * 2
+        w_min = np.zeros(len(training_set[0]))
+        while self.error > 0 and iteration < self.limit:
+            iteration += 1
+            i_x = np.random.randint(0, p - 1)  # antes decia 1 y p
+            excitation = np.dot(training_set[i_x], w)
+            activation = SimplePerceptron.sign(excitation)
+            for i in range(0, len(w)):
+                delta_w[i] = n * (correct_output[i_x] - activation) * training_set[i_x][i]
+                # aca no entiendo que x[i_x] tomar porque seria un array [-1, 1] ponele
+                w[i] += delta_w[i]
+            self.error = self.calculate_error(training_set, correct_output, w, p)
             if self.error < min_error:
                 min_error = self.error
                 w_min = w
-            
-        if i == self.cota:
+
+        if iteration == self.limit:
             w = w_min
         print(w)
         return w
 
+    @staticmethod
+    def sign(n):
+        return 1 if math.isclose(n, 0, rel_tol=1e-5) else -1
 
-    def calculate_error(self, x, y, w, p):
-        return 0.001
-    
-    def calculate_h(self,try_i_x, w):
-        h = 0
-        for i in range(0, len(w)):
-            h = try_i_x[i] * w[i]
-        return h
-
+    # x -> conjunto de entrenamiento
+    # y -> salida deseada
+    @staticmethod
+    def calculate_error(x, y, w, p):
+        error = 0
+        for i in range(p):
+            output = SimplePerceptron.sign(np.dot(x[i], w))
+            error += abs(output - y[i])
+        return error
