1376cff50f4406ebb792ad5ad34c8f51aba25c05
srosati <srosati@itba.edu.ar>
2022-04-23 15:24:45 -0300

    time ejercico obligatorio

--- EjObligatorio1/main.py
+++ EjObligatorio1/main.py
@@ -1,6 +1,7 @@
 from solver import Solver
+import time
 
-def print_result(result):
+def print_result(result, time):
     arr = result[0]
     W = [arr[i] for i in range(0,3)]
     w = [[], []]
@@ -8,6 +9,7 @@ def print_result(result):
     w[1] = [arr[i] for i in range(6,9)]
     w0 = [arr[i] for i in range(9,11)]
 
+    print("Time:", time)
     print("Optimal solution")
     print("- W:", W)
     print("- w:", w)
@@ -19,14 +21,17 @@ inset = [[4.4793, -4.0765, -4.0765], [-4.1793, -4.9218, 1.7664], [-3.9429, -0.76
 correct_output = [0,1,1]
 
 print("Gradient Descent: ")
+start_time = time.time()
 result = Solver(inset, correct_output).gradient_minimizer()
-print_result(result)
+print_result(result, time.time() - start_time)
 
 print("Conjugate Gradient: ")
+start_time = time.time()
 result = Solver(inset, correct_output).conjugate_gradient_minimizer()
-print_result(result)
+print_result(result, time.time() - start_time)
 
 print("ADAM:")
+start_time = time.time()
 result = Solver(inset, correct_output).adam_minimizer()
-print_result(result)
+print_result(result, time.time() - start_time)
 
