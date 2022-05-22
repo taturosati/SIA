5eaf4a26cc3caeef612f556ce05b8ff14b40f7fa
srosati <sar@ltfargentina.com>
2022-05-22 14:47:30 -0300

    Ej obligatorio 2
    Co-authored-by: Matías Federico Lombardi <matiaslombardi@users.noreply.github.com>
    Co-authored-by: Patrick Dey <patrickmdey@users.noreply.github.com>

---
+++ EjObligatorio2/main.py
@@ -0,0 +1,30 @@
+from pca import pca
+import csv
+import numpy as np
+
+
+with open('europe.csv', newline='') as csvfile:
+    # create np array with csv rows
+    rows = np.array(list(csv.reader(csvfile, delimiter=',')))
+    headers = np.array(rows[0][1:])
+    rows = rows[1:] # ignoro headers
+    countries = [row[0] for row in rows] # me quedo con los paises para poner los labels
+    rows = np.array([row[1:] for row in rows]) # elimino pais
+    
+    csvfile.close()
+
+    # Initialize to reduce the data up to the number of componentes that explains 95% of the variance.
+    model = pca(n_components=0.95, normalize=True)
+
+    print(headers)
+    # Fit transform
+    results = model.fit_transform(rows, col_labels=headers, row_labels=countries)
+
+    # Plot explained variance
+    fig, ax = model.plot()
+
+    # Scatter first 2 PCs
+    fig, ax = model.scatter()
+
+    # Make biplot with the number of features
+    fig, ax = model.biplot()
